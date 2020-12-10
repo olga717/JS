063bb92858b9b25db9a8d1d4a4f1abcb37deb055
olga717 <74139091+olga717@users.noreply.github.com>
2020-12-10 19:04:41 +0300

    Function
    Создала ф-цию которая берет значение у внешней переменной

--- fun.js
+++ fun.js
@@ -13,12 +13,23 @@ myFun(); /*вызов ф-ции*/
 /*Локальная переменная
 видна только внутри ф-ции*/ 
 
-function showMessage() {
+/*function showMessage() {
     
     let message = "Привет, я JavaScript!"; // локальная переменная
     alert( message );
   }
   
-showMessage(); // Привет, я JavaScript!
+showMessage(); // Привет, я JavaScript!*/
   
 /*alert( message ); Не будет выводить т.к локал-ая*/ 
+
+/*Ф-ция имеет доступ к внешним переменным и
+может менят их значения */
+let userName = 'Вася'; /*внешняя переменная*/
+
+function showMessage() {
+  let message = 'Привет, ' + userName; /*локальная пере-ая  */
+  alert(message);
+}
+
+showMessage();
