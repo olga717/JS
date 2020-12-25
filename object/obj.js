17d9b1e8af98dd7f173ebf85e19e0ba3cdcd221e
olga717 <74139091+olga717@users.noreply.github.com>
2020-12-25 17:53:22 +0300

    in, цикл  for in, упорядочение
    порядок свойств

--- object/obj.js
+++ object/obj.js
@@ -14,6 +14,8 @@ alert(obj.ключ3 ключ)//доступ к
 //значению не работает
 //можно через []
 let user = {};
+
+
 // присваивание значения свойству
 user["likes birds"] = true;
 // получение значения свойства
@@ -55,3 +57,91 @@ const user = {
     lena.name = 'Elena'
     
     const str = new String('I am string')
+
+    //
+    let user = {
+        name: "John",
+        age: 30
+      };
+      let key = prompt("Что вы хотите узнать о пользователе?", "name");
+
+    // доступ к свойству через переменную
+    alert( user[key] ); // John (если ввели "name")
+      //через точку не работает
+      /*let key = "name";
+      alert( user.key ); // undefined*/
+//[] для создания вычисляемого с-ва в литеральной нотации
+        let fruit = prompt("Какой фрукт купить?", "apple");
+
+        let bag = {
+            [fruit]: 5, // имя св-а взято из п-ой fruit
+            };
+
+        alert( bag.apple ); // 5, если fruit="apple"
+    //для свойств объектов допустимы имена зарез-ых слов
+let obj = {
+    for: 1,
+    let: 2,
+    return: 3
+  };
+  
+  alert( obj.for + obj.let + obj.return );  // 6 
+  
+  
+  //__proto__не можем уст-ить его в необъектное значение:
+  let obj = {};
+  obj.__proto__ = 5; // присвоим число
+  alert(obj.__proto__); // [object Object]
+
+
+  //оператор "in" для проверки есть ли св-ва в объекте.
+let user = { name: "John", age: 30 };
+
+alert( "age" in user ); // true, user.age существует
+alert( "blabla" in user ); // false, user.blabla не существует
+
+
+// цикл for..in для перебора св-в объекта
+let user = {
+    name: 'John',
+    age: 30,
+    isAdmin: true
+};
+
+for (let key in user) {
+    alert (key);//name, age. isAdmin
+    alert ( user[key] );//'John', 30,true
+}
+//Упорядочение свойств объекта
+let codes = {
+    "49": "Германия",
+    "41": "Швейцария",
+    "44": "Великобритания",
+    // ..,
+    "1": "США"
+  };
+  
+  for (let code in codes) {
+    alert(code); // 1, 41, 44, 49
+  }
+  //это целочисленные св-ва, поэтому в порядке возростания
+  //делаем их нецелочисл. добавив +
+  let codes = {
+    "+49": "Германия",
+    "+41": "Швейцария",
+    "+44": "Великобритания",
+    // ..,
+    "+1": "США"
+  };
+  
+  for (let code in codes) {
+    alert( +code ); // 49, 41, 44, 1
+  }
+  //получаем в порядке написания
+
+//задание
+  let user = {};
+      user.name = 'John';
+      user.surname ='Smith';
+      user.name = 'Pete';
+  delete user.name;
